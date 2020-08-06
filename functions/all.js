9b4637e6368b8788ff3eddb42cf0556d0ef3e59a
adesche <37812488+adesche@users.noreply.github.com>
2020-08-06 13:49:29 -0700

    Update all.js

--- functions/all.js
+++ functions/all.js
@@ -1,14 +1,14 @@
 module.exports = {
-    syncroles: function (discord_id, site_token, site_url) {
+    syncroles: async function (discord_id, site_token, site_url) {
         const Discord = require('discord.js');
         const axios = require('axios');
         const client = new Discord.Client();
     
         try {
-            const response = axios.get(site_url + '/api/data/discord/?discord_id=' + discord_id + '&key=' + site_token);
+            const response = await axios.get(site_url + '/api/data/discord/?discord_id=' + discord_id + '&key=' + site_token);
             console.log(response);
-        } catch (err) {
-            console.log(err);
+        } catch (error) {
+            console.log(error);
             message.reply("**Oops...** an error occured.");
         }
     }
